9866e3e9c7be32c06f6b75238d6d1060f9e30570
Charlotte Evans <charlotteevans543@gmail.com>
2024-05-13 16:22:41 +0200

    Created component for navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {AppFooterComponent} from "./app-footer/app-footer.component";
 import {MenuComponent} from "./menu/menu.component";
+import {NavbarComponent} from "./navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AppFooterComponent, MenuComponent],
+  imports: [RouterOutlet, AppFooterComponent, MenuComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import {RouterLink} from "@angular/router";
+import {NavbarLinkComponent} from "../navbar-link/navbar-link.component";
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink,
+    NavbarLinkComponent
+  ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
 
+
+  isMenuOpen = false;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }
